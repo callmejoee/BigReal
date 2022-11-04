175f5b93b4a07edcaaf6a944b3d623e03669708b
Youssef Karam Muhammad Farag <92326279+callmejoee@users.noreply.github.com>
2022-11-05 00:38:31 +0200

    first 3 functions

--- BigReal.cpp
+++ BigReal.cpp
@@ -1,11 +1,83 @@
 #include <iostream>
+#include <string>
 #include "BigDecimalIntClass.h"
 using namespace std;
+class BigReal {
+private:
+    BigDecimalInt whole;
+    BigDecimalInt fraction;
+    string whole_string;
+    string fraction_string;
+    string number;
+    char sign = '+';
+public:
+    BigReal(double realNumber = 0.0){
 
+        // convert number to string
+        number = to_string(realNumber);
+
+        // check if number is negative and remove sign
+        if (number[0] == '-') {
+            sign = '-';
+            number.erase(0, 1);
+        }
+        else if (number[0] == '+') {
+            number.erase(0, 1);
+        }
+
+        // split number into whole and fraction
+        int j = 0;
+        for(int i = 0; i < number.length(); i++){
+            if( j == 0 && number[i] != '.'){
+                whole_string += number[i];
+            }
+            else if(number[i] == '.'){
+                j = 1;
+            }
+            else if(j == 1){
+                fraction_string += number[i];
+            }
+
+        }
+        // convert string to BigDecimalInt
+        whole = BigDecimalInt(whole_string);
+        fraction = BigDecimalInt(fraction_string);
+
+    };
+    BigReal(string realNumber){
+        // check if number is negative and remove sign
+        if (realNumber[0] == '-') {
+            sign = '-';
+            realNumber.erase(0, 1);
+        }
+        else if (realNumber[0] == '+') {
+            realNumber.erase(0, 1);
+        }
+
+        // split number into whole and fraction
+        int j = 0;
+        for(int i = 0; i < realNumber.length(); i++){
+            if( j == 0 && realNumber[i] != '.'){
+                whole_string += realNumber[i];
+            }
+            else if(realNumber[i] == '.'){
+                j = 1;
+            }
+            else if(j == 1){
+                fraction_string += realNumber[i];
+            }
+
+        }
+        // convert string to BigDecimalInt
+        whole = BigDecimalInt(whole_string);
+        fraction = BigDecimalInt(fraction_string);
+    };
+    BigReal(BigDecimalInt bigInteger){
+        whole = bigInteger;
+        fraction = BigDecimalInt("0");
+    };
+};
 int main() {
-    BigDecimalInt a("123456789012345678901234567890");
-    BigDecimalInt b("987654321098765432109876543210");
-    BigDecimalInt c = a + b;
-    cout << c << endl;
+    BigReal a(-123.456);
     return 0;
 }
