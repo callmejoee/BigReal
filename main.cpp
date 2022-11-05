d3c164eb4e44a7845afbdc1acd13949773136dce
Yousef M. Eldaly <94533646+YousefEldaly@users.noreply.github.com>
2022-11-05 20:27:16 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "BigDecimalIntClass.h"
+#include "BigDecimalIntClass.cpp"
+#include "BigRealClass.h"
+
+
+int main() {
+    BigReal a("-123.456");
+    return 0;
+}
